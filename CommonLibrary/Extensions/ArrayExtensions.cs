cc944d4012b9d5fbdc69eea62e0ff2caa71e3fbd
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2024-03-07 22:49:36 -0600

    good stuff

---
+++ CommonLibrary/Extensions/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static bool IsNullOrEmpty(this object[]? array)
+        {
+            return (array is null || array.Length == 0);
+        }
+    }
+}
